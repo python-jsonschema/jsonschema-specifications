bcb17d08ef192959f9bec7775b0754f1a6c147e8
Julian Berman <Julian@GrayVines.com>
2025-10-26 08:06:19 -0400

    Update supported Python versions in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -16,8 +16,8 @@ REQUIREMENTS_IN = [  # this is actually ordered, as files depend on each other
     (path.parent / f"{path.stem}.in", path) for path in REQUIREMENTS.values()
 ]
 
-SUPPORTED = ["3.9", "3.10", "pypy3.10", "3.11", "3.12", "3.13"]
-LATEST = "3.12"  # until 3.13 matures
+SUPPORTED = ["3.10", "pypy3.11", "3.11", "3.12", "3.13", "3.14"]
+LATEST = SUPPORTED[-1]
 
 nox.options.default_venv_backend = "uv|virtualenv"
 nox.options.sessions = []
